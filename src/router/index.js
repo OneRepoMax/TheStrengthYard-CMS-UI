8b45dd3fdf24a772b415d190681301d8a3b83a80
absolutelynoot <isalsamudra@hotmail.com>
2023-08-15 08:47:46 +0800

    Added routing

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,22 @@ const routes = [
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
       },
+      {
+        path: 'login',
+        name: 'Login',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "home" */ '@/views/Login.vue'),
+      },
+      {
+        path: 'register',
+        name: 'Register',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "home" */ '@/views/Registration.vue'),
+      },
     ],
   },
 ]
---
+++ src/views/Registration.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        Registration page
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
