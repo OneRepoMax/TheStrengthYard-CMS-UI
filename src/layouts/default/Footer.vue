a5802f5baefe58007bc70ce4dae143a20e88305a
absolutelynoot <isalsamudra@hotmail.com>
2023-08-15 20:40:57 +0800

    added common components

---
+++ src/layouts/default/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-footer class="d-flex flex-column pa-0">
+
+        <div class="bg-teal d-flex w-100 align-center">
+            <p class="px-4 py-2 text-body">The Strength Yard - Strength and Conditioning Gym Singapore, Copyright All Rights
+                Reserved © 2014 -
+                {{ new Date().getFullYear() }}</p>
+            <v-spacer></v-spacer>
+            <v-row>
+                <v-col>
+                    <a class="text-white" href="https://www.thestrengthyard.com/terms-conditions-strength-conditioning-gym-singapore/">Terms &
+                        Conditions</a>
+                </v-col>
+                <v-col>
+                    <a class="text-white" href="https://www.thestrengthyard.com/privacy-policy-strength-yard/">Privacy Policy</a>
+                </v-col>
+            </v-row>
+
+        </div>
+
+    </v-footer>
+</template>
+
+<script>
+
+
+</script>
+
+<style lang="scss" scoped></style>
