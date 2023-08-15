d595a8a09cb644b3cd79c0922afc1d3fe8773ee8
absolutelynoot <isalsamudra@hotmail.com>
2023-08-15 08:59:48 +0800

    added dark theme component

---
+++ src/components/DarkMode.vue
@@ -0,0 +1,25 @@
+<template>
+        <v-switch
+            hide-details
+            inset
+            :label="`Switch to ${theme.global.name.value.toString()}`"
+            @click="toggleTheme">
+        </v-switch>
+</template>
+
+<script>
+
+import { useTheme } from 'vuetify'
+
+export default {
+  setup () {
+    const theme = useTheme()
+
+    return {
+      theme,
+      toggleTheme: () => theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+    }
+  },
+}
+
+</script>
