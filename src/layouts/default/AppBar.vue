9a2dee2dfce7abdf03ce44e551de945a4c82cb31
absolutelynoot <isalsamudra@hotmail.com>
2023-08-15 20:40:57 +0800

    added common components

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,40 +1,27 @@
 <template>
   <v-app-bar :elevation="2">
-
-    <v-img class="mx-2 ms-5" src="src/assets/the-strength-yard-logo-main.svg" max-height="50" max-width="50"
+    
+    <v-container class="d-flex">
+      <template class="d-flex d-md-none">
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      </template>
+      <v-img class="mx-5 pa-3" src="src/assets/the-strength-yard-logo-main.svg" max-height="50" max-width="50"
       contain></v-img>
-    <v-app-bar-title class="font-weight-medium">{{ appTitle }}</v-app-bar-title>
-    <v-spacer></v-spacer>
-    <template class="d-none d-md-block">
-      <DarkMode />
-      <v-btn flat v-for="item in menuItems" :key="item.title" :to="item.path">
-        <v-icon left dark>{{ item.icon }}</v-icon>
-        {{ item.title }}
-      </v-btn>
-    </template>
+        <v-app-bar-title class="font-weight-medium align-center text-capitalize my-auto">{{ appTitle }}</v-app-bar-title>
 
-    <!-- <v-toolbar app>
-      <span class="hidden-sm-and-up">
-        <v-toolbar-side-icon @click="sidebar = !sidebar">
-        </v-toolbar-side-icon>
-      </span>
-      <v-toolbar-title>
-        <router-link to="/" tag="span" style="cursor: pointer">
-          {{ appTitle }}
-        </router-link>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-toolbar-items class="hidden-xs-only">
-        <v-btn
-          flat
-          v-for="item in menuItems"
-          :key="item.title"
-          :to="item.path">
-          <v-icon left dark>{{ item.icon }}</v-icon>
-          {{ item.title }}
-        </v-btn>
-      </v-toolbar-items>
-    </v-toolbar> -->
+        <template class="d-none d-md-block">
+          <dark-mode />
+          <v-btn flat v-for="item in menuItems" :key="item.title" :to="item.path" class="text-capitalize">
+            <v-icon left dark>{{ item.icon }}</v-icon>
+            {{ item.title }}
+          </v-btn>
+        </template>
+        
+
+
+    </v-container>
+
+
   </v-app-bar>
 </template>
 
--- src/layouts/default/Default.vue
+++ src/layouts/default/Default.vue
@@ -2,10 +2,12 @@
   <v-app>
     <default-bar />
     <default-view />
+    <default-footer />
   </v-app>
 </template>
 
 <script setup>
   import DefaultBar from './AppBar.vue'
   import DefaultView from './View.vue'
+  import DefaultFooter from './Footer.vue'
 </script>
