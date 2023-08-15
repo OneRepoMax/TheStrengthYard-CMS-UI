19181ad19881d5bcf7d076184fa2ee5e01d116d9
absolutelynoot <isalsamudra@hotmail.com>
2023-08-15 20:49:34 +0800

    refined footer

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,26 +1,38 @@
 <template>
-  <v-app-bar :elevation="2">
-    
+  <v-app-bar :elevation="2" color="black">
+
     <v-container class="d-flex">
       <template class="d-flex d-md-none">
         <v-app-bar-nav-icon></v-app-bar-nav-icon>
       </template>
       <v-img class="mx-5 pa-3" src="src/assets/the-strength-yard-logo-main.svg" max-height="50" max-width="50"
-      contain></v-img>
-        <v-app-bar-title class="font-weight-medium align-center text-capitalize my-auto">{{ appTitle }}</v-app-bar-title>
+        contain></v-img>
+      <v-app-bar-title class="font-weight-medium align-center text-capitalize my-auto">{{ appTitle }}</v-app-bar-title>
 
-        <template class="d-none d-md-block">
-          <dark-mode />
-          <v-btn flat v-for="item in menuItems" :key="item.title" :to="item.path" class="text-capitalize">
-            <v-icon left dark>{{ item.icon }}</v-icon>
-            {{ item.title }}
-          </v-btn>
-        </template>
-        
+      <div class="d-none d-md-block my-auto" v-if="!isLoggedIn">
+        <dark-mode />
+        <v-btn variant="flat" v-for="item in menuItems" :key="item.title" :to="item.path" class="text-capitalize mx-2"
+          :color="item.color">
+          <v-icon left dark class="me-2">{{ item.icon }}</v-icon>
+          {{ item.title }}
+        </v-btn>
+      </div>
 
+      <div class="d-none d-md-block my-auto" v-if="isLoggedIn" >
+        <v-btn variant="flat" v-for="item in loggedInMenuItems" :key="item.title" :to="item.path"
+          class="text-capitalize mx-2" :color="item.color">
+          {{ item.title }}
+        </v-btn>
+        <v-btn 
+          variant="flat" 
+          class="text-capitalize mx-2" 
+          color="red-lighten-1">
+          <v-icon left dark class="me-2">mdi-logout-variant</v-icon>
+          Logout
+        </v-btn>
+      </div>
 
     </v-container>
-
 
   </v-app-bar>
 </template>
@@ -34,10 +46,14 @@ export default {
   data() {
     return {
       appTitle: "Customer Management System",
+      isLoggedIn: false,
       sidebar: false,
       menuItems: [
-        { title: 'Home', path: '/', icon: 'mdi-home' },
-        { title: 'Login / Sign Up', path: '/account/login', icon: 'mdi-login-variant' },
+        { title: 'Login / Register', path: '/account/login', icon: 'mdi-login-variant', color: 'teal' },
+      ],
+      loggedInMenuItems: [
+        { title: 'Schedule', path: '/booking', color: 'transparent' },
+        { title: 'Book', path: '/book', color: 'transparent' },
       ]
     };
   },
--- src/layouts/default/Footer.vue
+++ src/layouts/default/Footer.vue
@@ -2,19 +2,30 @@
     <v-footer class="d-flex flex-column pa-0">
 
         <div class="bg-teal d-flex w-100 align-center">
-            <p class="px-4 py-2 text-body">The Strength Yard - Strength and Conditioning Gym Singapore, Copyright All Rights
-                Reserved © 2014 -
-                {{ new Date().getFullYear() }}</p>
-            <v-spacer></v-spacer>
-            <v-row>
-                <v-col>
-                    <a class="text-white" href="https://www.thestrengthyard.com/terms-conditions-strength-conditioning-gym-singapore/">Terms &
-                        Conditions</a>
-                </v-col>
-                <v-col>
-                    <a class="text-white" href="https://www.thestrengthyard.com/privacy-policy-strength-yard/">Privacy Policy</a>
-                </v-col>
-            </v-row>
+            <v-container>
+                <v-row class="d-flex d-columns align-center">
+                    <v-col-auto>
+                        <p class="text-body text-start">The Strength Yard - Strength and Conditioning Gym Singapore,
+                            Copyright All
+                            Rights
+                            Reserved © 2014 -
+                            {{ new Date().getFullYear() }}</p>
+                    </v-col-auto>
+                    <v-col class="text-end">
+                        <a class="text-white me-5"
+                            href="https://www.thestrengthyard.com/terms-conditions-strength-conditioning-gym-singapore/">Terms
+                            &
+                            Conditions</a>
+                            <a class="text-white" href="https://www.thestrengthyard.com/privacy-policy-strength-yard/">Privacy
+                            Policy</a>
+                    </v-col>
+                    
+                        
+                    
+                </v-row>
+
+            </v-container>
+
 
         </div>
 
