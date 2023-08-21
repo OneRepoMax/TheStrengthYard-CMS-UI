5300139f324a33b137d826688d6d578e40956225
absolutelynoot <isalsamudra@hotmail.com>
2023-08-21 16:06:50 +0800

    updated app bar

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,8 +1,8 @@
 <template>
     <v-navigation-drawer v-model="drawer" location="bottom" temporary theme="dark" style="height: 500px;">
         <v-list>
-            <v-list-item prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg" :title="userStore.firstName + userStore.lastName"
-                :subtitle="userStore.emailAddress"></v-list-item>
+            <v-list-item prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+                :title="userStore.firstName + userStore.lastName" :subtitle="userStore.emailAddress"></v-list-item>
         </v-list>
         <v-list-item>
             <v-btn block variant="flat" color="red-lighten-1" @click="logout()">
@@ -21,14 +21,10 @@
 
 
         <v-list nav>
-            <v-list-item v-for="item in menuItems" 
-                :key="item.title" 
-                :to="item.path"
-                :prepend-icon="item.icon"
-                :title="item.title"
-                >
-                
-        </v-list-item>
+            <v-list-item v-for="item in menuItems" :key="item.title" :to="item.path" :prepend-icon="item.icon"
+                :title="item.title">
+
+            </v-list-item>
 
         </v-list>
     </v-navigation-drawer>
@@ -47,10 +43,30 @@
                     :color="item.color">
                     {{ item.title }}
                 </v-btn>
-                <v-btn variant="flat" class="mx-2" color="red-lighten-1" @click="logout">
-                    <v-icon left dark class="me-2">mdi-logout-variant</v-icon>
-                    Logout
-                </v-btn>
+
+                <v-menu location="bottom">
+                    <template v-slot:activator="{ props }">
+                        <v-btn v-bind="props" icon="https://randomuser.me/api/portraits/women/85.jpg">
+                            <v-avatar image="https://randomuser.me/api/portraits/women/85.jpg"></v-avatar>
+                        </v-btn>
+                    </template>
+                    <v-list theme="dark">
+                        <v-list-item prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+                            :title="userStore.firstName + userStore.lastName" :subtitle="userStore.emailAddress"
+                            class="mb-3">
+                        </v-list-item>
+                        <v-divider></v-divider>
+
+                        <v-list-item v-for="(item, index) in profileItems" :key="index" :to="item.path">
+                            {{ item.title }}
+                        </v-list-item>
+                        <v-list-item  @click="logout">
+                            <v-icon left dark class="me-2" color="red-lighten-1">mdi-logout-variant</v-icon>
+                            Logout
+                        </v-list-item>
+                    </v-list>
+                </v-menu>
+
             </div>
         </v-container>
     </v-app-bar>
@@ -64,7 +80,7 @@ import { useUserStore } from '@/store/user'
 
 export default {
     name: "App",
-    setup(){
+    setup() {
         const userStore = useUserStore()
 
         return { userStore }
@@ -77,9 +93,13 @@ export default {
             isLoggedIn: false,
             menuItems: [
                 { title: 'Home', path: '/', icon: "mdi-home", color: 'transparent' },
-                { title: 'Schedule', icon:"mdi-calendar-badge", path: '/schedule', color: 'transparent' },
-                { title: 'Book', icon:"mdi-login-variant", path: '/book', color: 'transparent' },
+                { title: 'Schedule', icon: "mdi-calendar-badge", path: '/schedule', color: 'transparent' },
+                { title: 'Book', icon: "mdi-login-variant", path: '/book', color: 'transparent' },
             ],
+            profileItems: [
+                { title: 'View Profile', path: '/account/profile', icon: "mdi-home", color: 'transparent' },
+                { title: 'Edit Profile', path: '/account/edit-profile', icon: "mdi-home", color: 'transparent' }
+            ]
         };
     },
     watch: {
@@ -88,7 +108,7 @@ export default {
         },
     },
     methods: {
-        logout(){
+        logout() {
 
             // clear global store
             this.userStore.$reset()
