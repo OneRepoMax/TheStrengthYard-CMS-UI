ea05abdaac0a3027abe29595b53ed85522c970c9
Faisal Samudra <isalsamudra@hotmail.com>
2023-08-21 10:59:47 +0800

    code refactoring

--- src/layouts/default/LoginDefault.vue
+++ src/layouts/default/LoginDefault.vue
@@ -1,24 +1,33 @@
 <template>
-  <v-app>
-    <default-bar />
-    <div class="login-default">
-        <default-view />
-    </div>
-    <default-footer />
-  </v-app>
+    <v-app>
+        <default-bar />
+        <div class="default-bg py-8">
+            <div class="login-default">
+                <default-view />
+            </div>
+        </div>
+        <default-footer />
+    </v-app>
 </template>
 
 <script setup>
-  import DefaultBar from '@/components/AppBar.vue'
-  import DefaultView from './View.vue'
-  import DefaultFooter from '@/components/Footer.vue'
+import DefaultBar from '@/components/AppBar.vue'
+import DefaultView from './View.vue'
+import DefaultFooter from '@/components/Footer.vue'
 </script>
 
 <style>
 
-.login-default{
+.login-default {
     max-width: 1280px;
-    align-self: center;
+    align-content: center;
+    margin: 0 auto;
+    display: flex;
+    justify-content: center;
+}
+
+.default-bg {
+    background-color: #ECEFF1;
 }
 
 </style>
