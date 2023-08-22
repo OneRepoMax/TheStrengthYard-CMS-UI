be0c4e37d86bbbc03e1000ed37219d558ff2eb32
thyngchuansik <darrenthyng@hotmail.com>
2023-08-22 13:57:36 +0800

    first commit for edit profile

---
+++ src/views/EditProfile.vue
@@ -0,0 +1,95 @@
+
+<template>
+    <v-main>
+        <v-container fluid class="login-bg" justify="center">
+            <v-row justify="center" class="h-100" align="center">
+                <v-col cols="12" lg="4" md="5">
+                    <v-card elevation="3">
+                        <v-card-title class="text-center">
+                            <v-card-title>Edit Profile</v-card-title>
+                            <v-avatar size="80" color="surface-variant" @click="openFileInput">
+                            <v-img v-if="selectedFile" :src="selectedFile" alt="Avatar" max-height="80px"></v-img>
+                            <v-icon v-else>mdi-camera</v-icon>
+                            </v-avatar>
+                            <input
+                            ref="fileInput"
+                            type="file"
+                            style="display: none"
+                            accept="image/*"
+                            @change="handleFileUpload"
+                            />
+                            <v-card-subtitle class ="mt-3 mb-3"> Profile Picture </v-card-subtitle>
+                            
+                        </v-card-title>
+                        <v-card-text>
+                            <v-text-field clearable hide-details="auto" class="mb-3" label="First Name" v-model="firstName" variant="outlined"></v-text-field>
+                            <v-text-field clearable hide-details="auto" class="mb-3" label ="Last Name" v-model="lastName" variant="outlined"></v-text-field>
+                            <v-text-field clearable hide-details="auto" class="mb-3" label ="Email" v-model="email" variant="outlined"></v-text-field>
+                            <v-text-field clearable hide-details="auto" class="mb-3" label ="Address" v-model="address" variant="outlined"></v-text-field>
+                            <v-btn color="teal" block type="submit" size="large">Update Profile</v-btn>
+                        </v-card-text>
+                        
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-main>
+</template>
+
+<script>
+import { useUserStore } from '@/store/user'
+
+export default {
+    name: 'LoginForm',
+    setup() {
+        const userStore = useUserStore()
+
+        return {
+            userStore
+        }
+    },
+    data(){
+        return {
+            firstName: "David",
+            lastName: "David",
+            email: "",
+            address: "",
+            password: "",
+            selectedFile: null,
+            profilePicture: null,
+        }
+    },
+    methods:{
+        openFileInput() {
+      // Trigger the click event of the hidden file input element when the avatar is clicked
+      this.$refs.fileInput.click();
+    }
+    ,
+    handleFileUpload(event) {
+      const file = event.target.files[0];
+      
+      if (file) {
+        // Handle the file upload logic here.
+        // You can send the file to a server for processing or display it locally.
+        console.log('Selected file:', file);
+
+        // Optionally, if you want to display the selected image locally:
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+
+        reader.onload = () => {
+          this.selectedFile = reader.result;
+        };
+
+        this.profilePicture = file
+      }
+    },
+    }
+}
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
