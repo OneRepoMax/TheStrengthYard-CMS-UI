3688f26a7a8cc1f4878786271aabb0fe6215b72a
Faisal Samudra <isalsamudra@hotmail.com>
2023-08-23 23:50:31 +0800

    added reset password

--- src/views/ResetPassword.vue
+++ src/views/ResetPassword.vue
@@ -1,54 +1,37 @@
 <template>
-  <v-main>
-    <v-container fluid class="login-bg" justify="center">
-      <v-row justify="center" class="h-100" align="center">
-        <v-col cols="12" lg="4" md="5">
-          <v-card>
-            <v-card-title class="text-center">
-              <v-img
-                src="@/assets/the-strength-yard-logo-main.svg"
-                max-height="150px"
-                class="ma-3"
-                inset
-              ></v-img>
-              <v-card-title>Reset Password</v-card-title>
-            </v-card-title>
-            <v-card-text>
-              <v-form @submit.prevent="login" validate-on="submit">
-                <v-text-field
-                  v-model="emailAddress"
-                  label="Email Address"
-                  :rules="rules"
-                  class="mt-3"
-                  required
-                ></v-text-field>
-        
-                <v-btn
-                  color="teal"
-                  block
-                  type="submit"
-                  size="large"
-                  :disabled="loading"
-                  :loading="loading"
-                  class="mt-3"
-                  >Reset Password</v-btn
-                >
-               
-              </v-form>
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-main>
+    <v-main>
+        <v-container fluid class="login-bg" justify="center">
+            <v-row justify="center" class="h-100" align="center">
+                <v-col cols="12" lg="4" md="5">
+                    <v-card>
+                        <v-card-title class="text-center">
+                            <v-img src="@/assets/the-strength-yard-logo-main.svg" max-height="150px" class="ma-3"
+                                inset></v-img>
+                            <v-card-title>Reset Password</v-card-title>
+                        </v-card-title>
+                        <v-card-text>
+                            <v-form @submit.prevent="resetPassword" validate-on="submit">
+                                <v-text-field v-model="emailAddress" label="Email Address" :rules="rules" class="mt-3"
+                                    required></v-text-field>
+
+                                <v-btn color="teal" block type="submit" size="large" :disabled="loading" :loading="loading"
+                                    class="mt-3">Reset Password</v-btn>
+
+                            </v-form>
+                        </v-card-text>
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-main>
 </template>
 
 <style scoped>
 .login-bg {
-  width: 100vw;
-  height: 100vh;
-  background-image: url("/src/assets/login_splash.jpg");
-  background-size: cover;
+    width: 100vw;
+    height: 100vh;
+    background-image: url("/src/assets/login_splash.jpg");
+    background-size: cover;
 }
 </style>
 
@@ -56,47 +39,49 @@
 import { useUserStore } from "@/store/user";
 
 export default {
-  name: "LoginForm",
-  setup() {
-    const userStore = useUserStore();
+    name: "LoginForm",
+    setup() {
+        const userStore = useUserStore();
 
-    return {
-      userStore,
-    };
-  },
-  data() {
-    return {
-      // temp state
-      emailAddress: "",
-      password: "",
-      loading: false,
-      rules: [
-        (value) => {
-          if (value) return true;
-
-          return "This field cannot be empty.";
-        },
-      ],
-    };
-  },
-  methods: {
-    async login() {
-      this.loading = true;
-      const response = await this.userStore.login(
-        this.emailAddress,
-        this.password
-      );
-      this.loading = false;
-
-      console.log(response);
-
-      if (response == null || response.status != 200) {
-        console.log("invalid login details");
-      } else {
-        console.log("Login Successful");
-        this.$router.push({ path: "/" });
-      }
+        return {
+            userStore,
+        };
     },
-  },
+    data() {
+        return {
+            // temp state
+            emailAddress: "",
+            password: "",
+            loading: false,
+            rules: [
+                (value) => {
+                    if (value) return true;
+
+                    return "This field cannot be empty.";
+                },
+            ],
+        };
+    },
+    methods: {
+        async resetPassword() {
+            try {
+                this.loading = true;
+                const response = await this.userStore.resetPassword(
+                    this.emailAddress,
+                );
+                this.loading = false;
+
+                console.log(response);
+
+                console.log("Reset password success");
+
+            }
+
+             catch(error) {
+            console.console.log("Reset password error: ", error);
+        }
+
+    },
+},
 };
 </script>
