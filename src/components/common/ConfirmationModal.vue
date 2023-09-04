2bddc4f64e792aa61ace64dd6f3f0212c25ff85a
Faisal Samudra <isalsamudra@hotmail.com>
2023-09-05 02:04:44 +0800

    added delete membership record

---
+++ src/components/common/ConfirmationModal.vue
@@ -0,0 +1,39 @@
+<template>
+    <v-dialog max-width="400px" @input="$emit('input', $event)">
+        <v-card :title="this.title">
+            <template v-slot:append>
+                <v-btn icon="$close" variant="text" @click="$emit('closeModal')"></v-btn>
+            </template>
+            <v-divider></v-divider>
+            <v-card-text class="text-center">
+                <v-icon size="130" :color="type">{{ icon }}</v-icon>
+            </v-card-text>
+            <v-card-text class="text-center">
+                {{ this.message }}
+            </v-card-text>
+            <v-divider></v-divider>
+            <v-card-text>
+                <v-row dense>
+                    <v-col cols="12" md="6">
+                        <v-btn block color="red" class="mb-5" @click="$emit('closeModal')">No</v-btn>
+                    </v-col>
+                    <v-col cols="12" md="6">
+                        <v-btn block color="black" class="mb-5" @click="$emit('confirmAction')">Yes</v-btn>
+                    </v-col>
+                </v-row>
+            </v-card-text>
+        </v-card>
+    </v-dialog>
+</template>
+    
+<script>
+export default {
+    props: {
+        value: Boolean,
+        title: String,
+        type: String,
+        icon: String,
+        message: String,
+    },
+}
+</script>
