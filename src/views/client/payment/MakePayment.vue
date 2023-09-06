9ca64407c813e3b955d76b4b998a9136135b907c
Faisal Samudra <isalsamudra@hotmail.com>
2023-09-06 20:14:07 +0800

    added payment

---
+++ src/views/client/payment/MakePayment.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    },
+    props: {
+        membership: Object
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
