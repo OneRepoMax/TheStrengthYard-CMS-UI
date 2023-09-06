75d041eb23360cbe8a879192ee9e59cf676b474b
Faisal Samudra <isalsamudra@hotmail.com>
2023-09-06 19:25:31 +0800

    added membership slider on home page

---
+++ src/components/membership/PurchaseMembershipCard.vue
@@ -0,0 +1,74 @@
+<template>
+  <v-hover v-slot="{ isHovering, props }">
+    <v-card
+      class="my-2 me-4"
+      max-width="250px"
+      min-height="540px"
+      height="550px"
+      :class="{ 'on-hover': isHovering }"
+      v-bind="props"
+      variant="flat"
+      @click="navigateToRoute('/next-route')"
+    >
+      <!-- navigateToRoute's route to change when new screen is built -->
+      <v-img
+        class="align-end text-white"
+        height="200"
+        :src="membership.Picture"
+        cover
+      >
+        <v-card-title>{{ membership.Type }}</v-card-title>
+      </v-img>
+      <v-card-title class="text-subtitle-1">
+        <p class="text-wrap">{{ membership.Title }}</p>
+      </v-card-title>
+      <v-card-subtitle class="text-wrap">{{
+        membership.Description
+      }}</v-card-subtitle>
+      
+      <v-card-subtitle class="my-2">
+        <v-icon color="error" icon="mdi-refresh" size="small"></v-icon>
+        <span class="mx-1">{{ membership.Type }}</span>
+      </v-card-subtitle>
+      <v-card-subtitle class="mb-2">
+        <v-icon color="error" icon="mdi-currency-usd" size="small"></v-icon>
+        <span class="mx-1">{{ membership.BaseFee }}</span>
+      </v-card-subtitle>
+    </v-card>
+  </v-hover>
+</template>
+
+<script>
+export default {
+  props: {
+    membership: Object,
+    membershipRecord: Array,
+  },
+  data() {
+    return {
+      loading: false,
+    };
+  },
+  methods: {
+    navigateToRoute(route) {
+      this.$router.push(route);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+
+.v-card {
+  transition: opacity 0.2s ease-in-out;
+}
+
+.v-card:hover {
+  cursor: pointer;
+}
+
+.v-card:not(.on-hover) {
+  opacity: 1;
+}
+</style>
---
+++ src/components/membership/PurchaseMembershipSlider.vue
@@ -0,0 +1,139 @@
+<template>
+    <v-card class="my-3" elevation="3">
+        <v-card-text>
+            <v-card-title>
+                Get your membership now!
+            </v-card-title>
+            <v-card-subtitle>
+                <p class="text-wrap mb-3">
+                    Here’s Our Gym Classes For Those Who Enjoy The Camaraderie Of Training In A Group While Getting
+                    Functionally Stronger!
+                </p>
+            </v-card-subtitle>
+            <v-divider></v-divider>
+        </v-card-text>
+        <v-card-text class="py-0">
+            <v-carousel :continuous="false" v-model="page" hide-delimiter-background cycle show-arrows hide-delimiters
+                height="420px">
+                <v-carousel-item v-for="(slice, index) in slicedMemberships" :key="index">
+                    <v-row dense>
+                        <v-col v-for="membership in slice" :key="membership.MembershipTypeId"
+                            :value="membership.MembershipTypeId" class="px-0">
+                            <v-hover v-slot="{ isHovering, props }"></v-hover>
+                            <v-card class="mx-5 my-2" :class="{ 'on-hover': isHovering }" variant="flat"
+                                @click="navigateToRoute('/membership/purchase-membership')" v-bind="props" height="400px">
+                                <v-img class="align-end text-white" max-height="150px" :src="membership.Picture" cover>
+                                </v-img>
+                                <v-card-title class="text-subtitle-1">
+                                    <p class="text-wrap">{{ membership.Title }}</p>
+                                </v-card-title>
+                                <v-card-subtitle class="mb-2">
+                                    <p class="text-wrap">{{ membership.Description }}</p>
+                                </v-card-subtitle>
+                                <v-card-subtitle class="mb-2">
+                                    <v-icon color="error" icon="mdi-refresh" size="small"></v-icon>
+                                    <span class="mx-1">{{ membership.Type }}</span>
+                                </v-card-subtitle>
+                                <v-card-subtitle class="mb-2">
+                                    <v-icon color="error" icon="mdi-currency-usd" size="small"></v-icon>
+                                    <span class="mx-1">{{ membership.BaseFee }}</span>
+                                </v-card-subtitle>
+                            </v-card>
+                        </v-col>
+                    </v-row>
+                </v-carousel-item>
+            </v-carousel>
+
+        </v-card-text>
+
+    </v-card>
+</template>
+
+<script>
+import { useMembershipStore } from "@/store/membership";
+
+export default {
+    props: {
+        membershipRecord: Array,
+    },
+    setup() {
+        const membershipStore = useMembershipStore();
+        return { membershipStore };
+    },
+    data() {
+        return {
+            membershipId: null,
+            loading: false,
+            membershipList: [],
+            page: 1,
+        };
+    },
+    mounted() {
+        this.getMembershipList();
+    },
+    methods: {
+        async getMembershipList() {
+
+            this.loading = true;
+            const response = await this.membershipStore.getAllMembership();
+            this.loading = false;
+
+            if (response.status == 200) {
+                this.membershipList = response.data;
+                console.log(this.MembershipList);
+            }
+
+        },
+        showModal() {
+            this.modal.show = true;
+            // this.membershipId = id
+        },
+        closeModal() {
+            this.modal.show = false;
+        },
+        navigateToRoute(route) {
+            this.$router.push(route);
+        },
+        isInMembershipRecord(membership) {
+            // Ensure this.membershipRecord is defined before using some
+            if (this.membershipRecord && this.membershipRecord.length > 0) {
+                return this.membershipRecord.some(
+                    (record) => record.MembershipTypeId === membership.MembershipTypeId
+                );
+            }
+            return false; // Return false if membershipRecord is undefined or empty
+        },
+    },
+    computed: {
+        slicedMemberships() {
+            // Slice the membership records into groups of three
+            const sliced = [];
+            for (let i = 0; i < this.membershipList.length; i += 2) {
+                sliced.push(this.membershipList.slice(i, i + 2));
+            }
+            return sliced;
+        },
+    },
+    components: {
+        // Modal,
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.subtext {
+    height: 50px;
+}
+
+.v-card {
+    transition: opacity 0.2s ease-in-out;
+}
+
+.v-card:hover {
+    cursor: pointer;
+}
+
+.v-card:not(.on-hover) {
+    opacity: 1;
+}
+</style>
