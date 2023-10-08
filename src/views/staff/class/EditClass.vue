9fcc981806ebc198b7be9b7ac0f287df897668e0
veinnaa <tioveinna@gmail.com>
2023-10-08 16:07:24 +0800

    Update Edit Class

---
+++ src/views/staff/class/EditClass.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <EditClass />
+    </div>
+</template>
+
+<script>
+import EditClass from "@/components/class/EditClass.vue"
+export default {
+    components: {
+        EditClass
+    },
+    setup () {
+        
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
