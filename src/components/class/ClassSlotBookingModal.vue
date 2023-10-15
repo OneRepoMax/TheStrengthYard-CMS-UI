4564e56e72748d4134117d4fc49af4935fb930ad
Faisal Samudra <isalsamudra@hotmail.com>
2023-10-16 03:15:31 +0800

    added view class slot booking list

--- src/components/class/ClassSlotBookingModal.vue
+++ src/components/class/ClassSlotBookingModal.vue
@@ -5,7 +5,10 @@
                 <v-btn icon="$close" variant="text" @click="this.$emit('closeModal')"></v-btn>
             </template>
             <v-divider></v-divider>
-            <v-card-item v-if="bookingList != null">
+            <v-card-item v-if="this.bookingList.length==0">
+                <v-alert type="info">There's no booking made for this class</v-alert>
+            </v-card-item>
+            <v-card-item v-if="getConfirmedBookingCount > 0">
                 <v-card-title class="mb-1"><v-chip prepend-icon="mdi-account-multiple" class="me-2"> <span
                             class="font-weight-medium">{{ getConfirmedBookingCount }}</span></v-chip>Confirmed
                     Attendees</v-card-title>
@@ -35,51 +38,52 @@
 
                             </div>
                             <v-spacer></v-spacer>
-                            <v-chip color="primary"><v-icon start icon="mdi-check"></v-icon>Booking Id: {{ booking.BookingId }}</v-chip>
+                            <v-chip color="primary"><v-icon start icon="mdi-check"></v-icon>Booking Id: {{ booking.BookingId
+                            }}</v-chip>
                         </div>
                         <v-divider class="mt-4"></v-divider>
                     </v-list>
                 </template>
 
-                <template v-if="getCancelledBookingCount > 0">
 
-                    <v-card-title class="mb-1 mt-2"><v-chip prepend-icon="mdi-account-cancel" class="me-2" color="red">
-                            <span class="font-weight-medium">{{ getCancelledBookingCount }}</span></v-chip>Cancelled
-                        Attendees</v-card-title>
-                    <v-card-subtitle class="mb-1">Listed below are the cancelled attendees</v-card-subtitle>
-                    <template v-for="booking in bookingList" :key="booking.BookingId">
-                        <v-list v-if="booking.Status == 'Cancelled'">
-                            <div class="d-flex w-100">
-                                <v-avatar v-if="booking.User.DisplayPicture" :size="50" :image="booking.User.DisplayPicture"
-                                    class="me-3"></v-avatar>
-                                <v-avatar v-else icon="mdi-account-circle" :size="50" class="me-3"></v-avatar>
+
+
+            </v-card-item>
+            <v-card-item v-if="getCancelledBookingCount > 0">
+                <v-card-title class="mb-1"><v-chip prepend-icon="mdi-account-cancel" class="me-2" color="red">
+                        <span class="font-weight-medium">{{ getCancelledBookingCount }}</span></v-chip>Cancelled
+                    Attendees</v-card-title>
+                <v-card-subtitle class="mb-1">Listed below are the cancelled attendees</v-card-subtitle>
+                <template v-for="booking in bookingList" :key="booking.BookingId">
+                    <v-list v-if="booking.Status == 'Cancelled'">
+                        <div class="d-flex w-100">
+                            <v-avatar v-if="booking.User.DisplayPicture" :size="50" :image="booking.User.DisplayPicture"
+                                class="me-3"></v-avatar>
+                            <v-avatar v-else icon="mdi-account-circle" :size="50" class="me-3"></v-avatar>
+                            <div>
+                                <div class="text-subtitle-1 font-weight-bold">{{ booking.User.FirstName }} {{
+                                    booking.User.LastName }}</div>
+                                <div class="text-subtitle-2 mb-2">{{ booking.User.EmailAddress }}</div>
                                 <div>
-                                    <div class="text-subtitle-1 font-weight-bold">{{ booking.User.FirstName }} {{
-                                        booking.User.LastName }}</div>
-                                    <div class="text-subtitle-2 mb-2">{{ booking.User.EmailAddress }}</div>
-                                    <div>
-                                        <v-chip v-if="booking.User.Gender == 'M'" prepend-icon="mdi-gender-male" size="small"
-                                            class="me-2">{{ formattedGender(booking.User.Gender) }}</v-chip>
-                                        <v-chip v-if="booking.User.Gender == 'F'" prepend-icon="mdi-gender-female"
-                                            size="small" class="me-2">{{ formattedGender(booking.User.Gender) }}</v-chip>
-                                        <v-chip v-if="booking.User.Gender == 'O'" prepend-icon="mdi-gender-non-binary"
-                                            size="small" class="me-2">{{ formattedGender(booking.User.Gender) }}</v-chip>
-                                        <v-chip prepend-icon="mdi-phone" @click.prevent="phoneCall(booking.User.ContactNo)"
-                                            size="small">{{
-                                                booking.User.ContactNo
-                                            }}</v-chip>
-                                    </div>
+                                    <v-chip v-if="booking.User.Gender == 'M'" prepend-icon="mdi-gender-male" size="small"
+                                        class="me-2">{{ formattedGender(booking.User.Gender) }}</v-chip>
+                                    <v-chip v-if="booking.User.Gender == 'F'" prepend-icon="mdi-gender-female" size="small"
+                                        class="me-2">{{ formattedGender(booking.User.Gender) }}</v-chip>
+                                    <v-chip v-if="booking.User.Gender == 'O'" prepend-icon="mdi-gender-non-binary"
+                                        size="small" class="me-2">{{ formattedGender(booking.User.Gender) }}</v-chip>
+                                    <v-chip prepend-icon="mdi-phone" @click.prevent="phoneCall(booking.User.ContactNo)"
+                                        size="small">{{
+                                            booking.User.ContactNo
+                                        }}</v-chip>
                                 </div>
-                                <v-spacer></v-spacer>
-                                <v-chip color="red">Booking Id: {{ booking.BookingId }}</v-chip>
-
                             </div>
-                            <v-divider class="mt-4"></v-divider>
-                        </v-list>
-                    </template>
+                            <v-spacer></v-spacer>
+                            <v-chip color="red">Booking Id: {{ booking.BookingId }}</v-chip>
+
+                        </div>
+                        <v-divider class="mt-4"></v-divider>
+                    </v-list>
                 </template>
-
-
             </v-card-item>
             <v-card-text>
                 <v-btn block color="teal" class="mb-5" @click="closeModal(path)">Close</v-btn>
@@ -111,7 +115,9 @@ export default {
     methods: {
         async getAllBookingByClassSlotId() {
             const response = await this.classStore.getAllBookingByClassSlotId(this.classSlotId);
-            this.bookingList = response.data
+            if (response.status == 200) {
+                this.bookingList = response.data
+            }
         },
         closeModal() {
             this.$emit('closeModal')
@@ -131,9 +137,11 @@ export default {
     },
     computed: {
         getConfirmedBookingCount() {
+            if (this.bookingList == null || this.bookingList.length == 0) return 0
             return this.bookingList.filter(booking => booking.Status == 'Confirmed').length
         },
         getCancelledBookingCount() {
+            if (this.bookingList == null || this.bookingList.length == 0) return 0
             return this.bookingList.filter(booking => booking.Status == 'Cancelled').length
         },
 
